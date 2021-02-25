8b2c0c408d92948edf5d8ab7960f6feca9480fac
Vijay Kumar <vk77093@gmail.com>
2021-02-25 12:20:28 +0530

    meta tag added for chocolates pages

--- app/Http/Controllers/Darkchocolates.php
+++ app/Http/Controllers/Darkchocolates.php
@@ -8,97 +8,175 @@ class Darkchocolates extends Controller
 {
     public function dark(){
         $title="Dark Chocolates";
-        return view ('pages.dark',compact('title'));
+        $title_content="aariafoods range of dark chocolate from min coca 55% to 75% pure coco";
+        $descriptionTag= "At Aaria, the “Best quality cocoa” is sourced from the “Best places in the world”. That is the reason why we
+        bring you only the very best of Single Origin Chocolates.from min coca 55% to 75% pure coca ingredients";
+        $keywords= "aariafoods dark chocolate, aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,
+        top exportor of chocolates in india, dark chocolate price in india, dark chocolates across globe";
+        return view ('pages.dark',compact('title','title_content','descriptionTag','keywords'));
     }
 
     public function dec1401(){
-        $title="Desc-1401";
-        return view('dark.dec1401',compact('title'));
+        $title= "DARK CHOCOLATE COUVERTURE-1401";
+        $title_content = "DARK CHOCOLATE COUVERTURE with Min. cocoa 55.00%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1401', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1401()
     {
-        $title = "Technical-specification-1401";
-        return view('dark.spec.spec-1401', compact('title'));
+        $title = "Technical-specification-DARK CHOCOLATE COUVERTURE-1401";
+        $title_content = "details technical specification for DARK CHOCOLATE COUVERTURE-1401";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1401', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
 
     public function dec1407()
     {
-        $title = "Desc-1407";
-        return view('dark.dec1407', compact('title'));
+        $title = "DARK CHOCOLATE COUVERTURE-1407";
+        $title_content = "DARK CHOCOLATE COUVERTURE with Min. cocoa 60.00%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1407', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1407()
     {
-        $title = "Technical-specification-1407";
-        return view('dark.spec.spec-1407', compact('title'));
+        $title = "Technical-specification-DARK CHOCOLATE COUVERTURE-1407";
+        $title_content = "details technical specification for aariafoods DARK CHOCOLATE COUVERTURE-1407";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1407', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function dec1413()
     {
-        $title = "Desc-1413";
-        return view('dark.dec1413', compact('title'));
+        $title = "DARK CHOCOLATE COUVERTURE-1413";
+        $title_content = "DARK CHOCOLATE COUVERTURE with Min. cocoa 74.00%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1413', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1413()
     {
-        $title = "Technical-specification-1413";
-        return view('dark.spec.spec-1413', compact('title'));
+        $title = "Technical-specification- DARK CHOCOLATE COUVERTURE-1413";
+        $title_content = "details technical specification for aariafoods DARK CHOCOLATE COUVERTURE-1413";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1413', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function dec1388()
     {
-        $title = "Desc-1388";
-        return view('dark.dec1388', compact('title'));
+        $title = "DARK CHOCOLATE ST. DOMINGO -1388";
+        $title_content = "aariafoods DARK CHOCOLATE ST. DOMINGO -1388 with min coca 70%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1388', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1388()
     {
-        $title = "Technical-specification-1388";
-        return view('dark.spec.spec-1388', compact('title'));
+        $title = "Technical-specification-DARK CHOCOLATE ST. DOMINGO -1388";
+        $title_content = "details technical specification for aariafoods DARK CHOCOLATE ST. DOMINGO -1388";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1388', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function dec1406()
     {
-        $title = "Desc-1406";
-        return view('dark.dec1406', compact('title'));
+        $title = "DARK CHOCOLATE ECUADOR-1406";
+        $title_content = "aariafoods DARK CHOCOLATE ECUADOR-1406 with min coca 73.00%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1406', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1406()
     {
-        $title = "Technical-specification-1406";
-        return view('dark.spec.spec-1406', compact('title'));
+        $title = "Technical-specification-DARK CHOCOLATE ECUADOR-1406";
+        $title_content = "details technical specification for aariafoods DARK CHOCOLATE ECUADOR-1406";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1406', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function dec1427()
     {
-        $title = "Desc-1427";
-        return view('dark.dec1427', compact('title'));
+        $title = "DARK CHOCOLATE PERU-1427";
+        $title_content = "aariafoods DARK CHOCOLATE PERU -1427 with min coca 73.00%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1427', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1427()
     {
-        $title = "Technical-specification-1427";
-        return view('dark.spec.spec-1427', compact('title'));
+        $title = "Technical-specification-DARK CHOCOLATE PERU-1427";
+        $title_content = "details technical specification for aariafoods DARK CHOCOLATE PERU-1427";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1427', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function dec1440()
     {
-        $title = "Desc-1440";
-        return view('dark.dec1440', compact('title'));
+        $title = "DARK CHOCOLATE VENEZUELA-1440";
+        $title_content = "aariafoods DARK CHOCOLATE VENEZUELA -1440 with min coca 74.00%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1440', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1440()
     {
-        $title = "Technical-specification-1440";
-        return view('dark.spec.spec-1440', compact('title'));
+        $title = "Technical-specification-DARK CHOCOLATE VENEZUELA-1440";
+        $title_content = "details technical specification for aariafoods DARK CHOCOLATE VENEZUELA-1440";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1440', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function dec1441()
     {
-        $title = "Desc-1441";
-        return view('dark.dec1441', compact('title'));
+        $title = "DARK CHOCOLATE MADAGASCAR-1441";
+        $title_content = "aariafoods DARK CHOCOLATE MADAGASCAR-1441 with min coca 74.00%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1441', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1441()
     {
-        $title = "Technical-specification-1441";
-        return view('dark.spec.spec-1441', compact('title'));
+        $title = "Technical-specification-DARK CHOCOLATE MADAGASCAR-1441";
+        $title_content = "details technical specification for aariafoods DARK CHOCOLATE MADAGASCAR-1441";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1441', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function dec1442()
     {
-        $title = "Desc-1442";
-        return view('dark.dec1442', compact('title'));
+        $title = "DARK CHOCOLATE JAMAICA-1442";
+        $title_content = "aariafoods DARK CHOCOLATE JAMAICA-1442 with min coca 72.00%";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.dec1442', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
     public function tech1442()
     {
-        $title = "Technical-specification-1442";
-        return view('dark.spec.spec-1442', compact('title'));
+        $title = "Technical-specification-DARK CHOCOLATE JAMAICA-1442";
+        $title_content = "details technical specification for aariafoods DARK CHOCOLATE JAMAICA-1442";
+        $descriptionTag = "Create wonderful chocolate delights for your customers and spoil them with a wide assortment of couverture chocolate available in dark, milk and white to the fresh ...";
+        $keywords = "aariafoods dark chocolate,  aaria, aria chocolates, dark chocolate in india, dark chocolates
+        manufacture in india, best and premium quality of chocolates, best coca beans chocolates, crea partnership chocolates,";
+        return view('dark.spec.spec-1442', compact('title', 'title_content', 'descriptionTag', 'keywords'));
     }
 }
